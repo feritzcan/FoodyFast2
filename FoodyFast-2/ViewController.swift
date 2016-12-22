4ae53c8f0010e74010cf21bb579114d170ae0a15
Ferit Özcan <feritzcan@Ferits-MacBook-Pro.local>
2016-12-22 18:27:06 +0300

    Initial Commit

---
+++ FoodyFast-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodyFast-2
+//
+//  Created by Ferit Özcan on 22/12/2016.
+//  Copyright © 2016 Potetic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
